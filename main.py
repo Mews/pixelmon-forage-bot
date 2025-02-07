b669539f089c39bde72e7374b80d304e38d2c752
Mews <ar754456@gmail.com>
2025-02-07 15:03:12 +0000

    Refactor anti afk option names

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def forage(g_press_num:int = 5):
     wait(0.5)
 
 
+def anti_afk():
+    pass
+
+
 
 class Options(ttk.Frame):
     def __init__(self, master, *args, **kwargs):
@@ -22,8 +26,8 @@ class Options(ttk.Frame):
             "g_presses": 5,                             # G presses per forage operation
             "g_press_delay": 0.1,                       # Seconds between G presses
             "forage_delay": 0.25,                       # Seconds between each forage operation
-            "do_anti_afk_jump": True,                   # Whether or not to jump every 20 forage operations
-            "forages_per_jump": 20,                     # How many forages it takes to do an afk jump
+            "do_anti_afk": True,                        # Whether or not to do the anti afk measure
+            "forages_per_anti_afk": 50,                 # How many forages it takes to do the anti afk
         }
 
         self.config(borderwidth=2, relief="groove")
@@ -34,8 +38,8 @@ class Options(ttk.Frame):
         self.create_g_presses_frame()
         self.create_g_press_delay_frame()
         self.create_forage_delay_frame()
-        self.create_anti_afk_jump_frame()
-        self.create_forages_per_jump_frame()
+        self.create_anti_afk_frame()
+        self.create_forages_per_anti_afk_frame()
 
     def create_g_presses_frame(self):
         self.g_presses_frame = ttk.Frame(self)
@@ -97,39 +101,39 @@ class Options(ttk.Frame):
 
         self.forage_delay_frame.grid(row=1, column=0, padx=10, pady=10)
 
-    def create_anti_afk_jump_frame(self):
-        self.anti_afk_jump_frame = ttk.Frame(self)
+    def create_anti_afk_frame(self):
+        self.anti_afk_frame = ttk.Frame(self)
 
-        self.master.options["do_anti_afk_jump"].set(self.default_options["do_anti_afk_jump"])
+        self.master.options["do_anti_afk"].set(self.default_options["do_anti_afk"])
 
-        self.anti_afk_jump_checkbutton = ttk.Checkbutton(
-            self.anti_afk_jump_frame,
-            text="Enable Anti-AFK jump",
-            variable=self.master.options["do_anti_afk_jump"],
+        self.anti_afk_checkbutton = ttk.Checkbutton(
+            self.anti_afk_frame,
+            text="Enable Anti-AFK",
+            variable=self.master.options["do_anti_afk"],
         )
-        self.anti_afk_jump_checkbutton.pack(side="bottom")
+        self.anti_afk_checkbutton.pack(side="bottom")
 
-        self.anti_afk_jump_frame.grid(row=0, column=1, padx=10, pady=10)
+        self.anti_afk_frame.grid(row=0, column=1, padx=10, pady=10)
     
-    def create_forages_per_jump_frame(self):
-        self.forages_per_jump_frame = ttk.Frame(self)
+    def create_forages_per_anti_afk_frame(self):
+        self.forages_per_anti_afk_frame = ttk.Frame(self)
 
-        self.master.options["forages_per_jump"].set(self.default_options["forages_per_jump"])
+        self.master.options["forages_per_anti_afk"].set(self.default_options["forages_per_anti_afk"])
 
         int_validate_cmd = self.register(self.validate_int_input)
 
-        self.forages_per_jump_entry = ttk.Entry(
-            self.forages_per_jump_frame,
+        self.forages_per_anti_afk_entry = ttk.Entry(
+            self.forages_per_anti_afk_frame,
             validate="key",
             validatecommand=(int_validate_cmd, "%P"),
             font=("", 8),
-            textvariable=self.master.options["forages_per_jump"],
+            textvariable=self.master.options["forages_per_anti_afk"],
         )
-        self.forages_per_jump_entry.pack(side="bottom")
+        self.forages_per_anti_afk_entry.pack(side="bottom")
 
-        ttk.Label(self.forages_per_jump_frame, text="Forages between afk jumps").pack(side="top")
+        ttk.Label(self.forages_per_anti_afk_frame, text="Forages between Anti-AFK").pack(side="top")
 
-        self.forages_per_jump_frame.grid(row=1, column=1, padx=10, pady=10)
+        self.forages_per_anti_afk_frame.grid(row=1, column=1, padx=10, pady=10)
     
     
     def validate_int_input(self, x:str):
@@ -218,8 +222,8 @@ class App(ttk.Window):
             "g_presses": ttk.StringVar(self),           # G presses per forage operation
             "g_press_delay": ttk.StringVar(self),       # Seconds between G presses
             "forage_delay": ttk.StringVar(self),        # Seconds between each forage operation
-            "do_anti_afk_jump": ttk.BooleanVar(self),   # Whether or not to jump every 20 forage operations
-            "forages_per_jump": ttk.StringVar(self),    # How many forages it takes to do an afk jump
+            "do_anti_afk": ttk.BooleanVar(self),   # Whether or not to jump every 20 forage operations
+            "forages_per_anti_afk": ttk.StringVar(self),    # How many forages it takes to do an afk jump
         }
 
         keyboard.add_hotkey("F7", self.hotkey_pressed)
@@ -266,10 +270,8 @@ class App(ttk.Window):
             forage(int(self.options["g_presses"].get()))
             self.forage_count += 1
 
-            if self.forage_count % int(self.options["forages_per_jump"].get()) == 0 and self.options["do_anti_afk_jump"].get():
-                for _ in range(3):
-                    pdi.press("space")
-                    wait(0.5)
+            if self.forage_count % int(self.options["forages_per_anti_afk"].get()) == 0 and self.options["do_anti_afk"].get():
+                anti_afk()
 
         self.after(100, self.loop)
     
